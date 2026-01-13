a3222a83a016189a795fb5bea8bc6cf78c8c69fa
choiminhyeok <edum96@gmail.com>
2026-01-13 10:58:07 +0900

    Add Contents Dto

---
+++ back/cinema/src/main/java/com/example/cinema/service/ContentService.java
@@ -0,0 +1,14 @@
+package com.example.cinema.service;
+
+
+import com.example.cinema.dto.ContentRequestDto;
+import com.example.cinema.dto.ContentResponseDto;
+
+public interface ContentService {
+
+    ContentResponseDto createContent(ContentRequestDto contentRequestDto);
+
+
+
+
+}
