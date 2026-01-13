b22ca9fc142b4df6daf27f45754536ca2cdcd3e4
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-13 16:56:21 +0900

    [refactor] ContentRepository

--- back/cinema/src/main/java/com/example/cinema/repository/ContentRepository.java
+++ back/cinema/src/main/java/com/example/cinema/repository/ContentRepository.java
@@ -1,12 +1,10 @@
 package com.example.cinema.repository;
 
 
-import io.swagger.v3.oas.annotations.media.Content;
+import com.example.cinema.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content,Long> {
-
-
 }
