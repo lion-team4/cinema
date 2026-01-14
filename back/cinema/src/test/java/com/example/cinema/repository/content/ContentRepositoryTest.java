42c5a07a82ca1f55755b82dc8ae6cee365a10501
corinB <gkdisrha2020@gmail.com>
2026-01-14 19:59:27 +0900

    content search done

--- back/cinema/src/test/java/com/example/cinema/repository/content/ContentRepositoryTest.java
+++ back/cinema/src/test/java/com/example/cinema/repository/content/ContentRepositoryTest.java
@@ -10,10 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 
 @SpringBootTest
@@ -43,14 +42,13 @@ class ContentRepositoryTest {
 
         Page<Content> contentPage = contentRepository.searchContent(request);
 
-        // 3. Then: 결과 검증 (AssertJ 사용 권장)
+
         assertThat(contentPage).isNotNull();
 
-        // 요청한 사이즈대로 페이징 설정이 되었는가?
+
         assertThat(contentPage.getSize()).isEqualTo(15);
 
-        // 실제 데이터가 조건에 맞게 필터링되어 돌아왔는가?
-        // (DB에 해당 태그를 가진 데이터가 15개 이상 있다고 가정할 때)
+
         assertThat(contentPage.getContent()).hasSize(15);
 
 
