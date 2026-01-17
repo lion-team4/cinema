6fd7644fec935b500cfe4e9a94386774b50e2c95
corinB <gkdisrha2020@gmail.com>
2026-01-17 21:04:05 +0900

    리뷰기능 테스트 찐 막

--- back/cinema/src/main/java/com/example/cinema/repository/watchHistory/WatchHistoryRepository.java
+++ back/cinema/src/main/java/com/example/cinema/repository/watchHistory/WatchHistoryRepository.java
@@ -1,14 +1,15 @@
 package com.example.cinema.repository.watchHistory;
 
+import com.example.cinema.entity.Content;
 import com.example.cinema.entity.ScheduleItem;
 import com.example.cinema.entity.User;
 import com.example.cinema.entity.WatchHistory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
